3002641435a41d9c3357cca89be594365f408cc9
Daniela CB <80734733+dgcb-4@users.noreply.github.com>
2024-05-02 18:08:15 +0100

    Check grammar and added more space .py
    Added more spaces between lines for better understanding of the code

--- Capstone_project_task_manager.py
+++ Capstone_project_task_manager.py
@@ -1,6 +1,6 @@
 # ---- CAPSTONE PROJECT ------------
 
-#=====importing libraries===========
+#===== Importing libraries===========
 import os
 from datetime import datetime, date
 
@@ -48,15 +48,15 @@ def reg_user():
     '''Add a new user to the user.txt file'''
     while True:
         # - Request input of a new username
-        new_username = input("New Username: ").strip()         # Remove whitespaces
+        new_username = input("New Username: ").strip()             # Remove whitespaces
 
-       if new_username in username_password:                   # Conditional to check if the user exists 
+       if new_username in username_password:                       # Conditional to check if the user exists 
             print("Error! Username already exists. Please choose another.")
             continue
         new_password = input("New Password: ")
         confirm_password = input("Confirm Password: ")
         
-        if new_password == confirm_password:                    # Conditional to check if both passwords match 
+        if new_password == confirm_password:                        # Conditional to check if both passwords match 
             username_password[new_username] = new_password
             with open("user.txt", "w") as out_file:
                 user_data = [f"{k};{username_password[k]}" for k in username_password]              # Format user data
@@ -88,8 +88,8 @@ def add_task():
             print("Invalid datetime format. Please use the format specified")
             
  
-    curr_date = date.today()                                        # Then get the current date
-    new_task = {                                                    # Adds task info in a dictionary 
+    curr_date = date.today()                                                # Then get the current date
+    new_task = {                                                            # Adds task info in a dictionary 
         "username": task_username,
         "title": task_title,
         "description": task_description,
@@ -146,7 +146,7 @@ def view_mine():
                 disp_str += f"Task Completed: \n {t['completed']}\n"
                 index += 1 
                 print(disp_str)
-                print("-" * 35)                                 # Separator
+                print("-" * 35)                                                 # Line Separator 
                 
  if not any(t['username'] == curr_user for t in task_list):
         print(f"\tThere isn't any task assigned for: {curr_user}")
@@ -158,7 +158,7 @@ def view_mine():
         
 # Check if the chosen number is valid 
     elif 0 < task_chose <= len(task_list) and task_chose in task_indices:
-      selected_task_index = task_indices[task_chose - 1]     #Provide the actual index of the task selected 
+      selected_task_index = task_indices[task_chose - 1]                     # Provide the actual index of the task selected 
       selected_task = task_list[selected_task_index]
 
       if selected_task['completed']:
@@ -207,10 +207,10 @@ def generating_reports():
   completed_tasks = sum(task["completed"] for task in task_list)
   uncompleted_tasks = total_tasks - completed_tasks
 
-  overdue_tasks = 0                   # Initializing the variable
-  today = datetime.today()            # Obtaining today's date for the comparison
-  for task in task_list:              # Loop to check all the tasks' dates
-    if not task["completed"] and task["due_date"] < today:   #  Conditonal to evaluate if task is overdue or not
+  overdue_tasks = 0                                           # Initializing the variable
+  today = datetime.today()                                    # Obtaining today's date for the comparison
+  for task in task_list:                                      # Loop to check all the tasks' dates
+    if not task["completed"] and task["due_date"] < today:    #  Conditonal to evaluate if task is overdue or not
       overdue_tasks += 1                
 
 
@@ -231,8 +231,8 @@ def generating_reports():
 """
 
 # Dictionary 'User tasks' from each user
-  user_tasks = {}                                  # Dictionary to store user's tasks 
-  for task in task_list:                           # Loop to check the user's tasks in the task_list 
+  user_tasks = {}                                              # Dictionary to store user's tasks 
+  for task in task_list:                                       # Loop to check the user's tasks in the task_list 
     user = task["username"]
     if user not in user_tasks:
       user_tasks[user] = {"assigned": 0, "completed": 0, "uncompleted": 0, "overdue": 0}
@@ -252,7 +252,7 @@ def generating_reports():
   user_report_content = f"\nUser Overview Report\n\n"
   user_report_content += f"Total Tasks: {total_tasks}\n\n"
 
-  for user, stats in user_tasks.items():                # Loop to obtain tasks status from each user
+  for user, stats in user_tasks.items():                    # Loop to obtain tasks status from each user
     assigned_tasks = stats["assigned"]
     completed_tasks = stats["completed"]
     uncompleted_tasks = stats["uncompleted"]
@@ -287,11 +287,12 @@ def generating_reports():
 # ======== MAIN CODE AND MENU =========
 ''' This code reads usernames and passwords from the user.txt file  allowing the user to login.
 '''
-# Main code to login 
-# Convert to a dictionary
+# Main code to log in
+
+# Converts to a dictionary
 username_password = {}
 for user in user_data:
-    username, password = user.split(';')
+    username, password = user.split(';')        
     username_password[username] = password
 
 logged_in = False
@@ -313,7 +314,7 @@ while not logged_in:
 
 #======== Main menu options =========
 while True:
-    # Presenting the menu to the user and ensuring that the input is converted to lowercase.
+    # Presenting the menu to the user and ensuring the input is converted to lowercase.
     print()
     menu = input('''Select one of the following options below:
 r - Registering a user
